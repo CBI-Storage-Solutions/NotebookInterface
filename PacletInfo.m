b7ecfa89972999ba554c65e43c14a315d55ac019
Mike Grigola <grigola3@gmail.com>
2022-10-19 18:17:55 -0500

    ++version

--- PacletInfo.m
+++ PacletInfo.m
@@ -2,7 +2,7 @@
 
 Paclet[
 	Name -> "NotebookInterface",
-	Version -> "1.0.0",
+	Version -> "1.0.1",
 	MathematicaVersion -> "11+",
 	Description -> "Supports the creation of user interfaces for notebook documents.",
 	Creator -> "Mike Grigola <Michael.Grigola@mcdermott.com>",
